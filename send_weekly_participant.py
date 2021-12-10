df18e30da3b6b46c329f77a907698eaaa56f9943
manfredmichael <koskosanrider@gmail.com>
2021-12-10 20:16:10 +0700

    Add separat gmail api class

---
+++ send_weekly_participant.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from tqdm import tqdm
+
+from modules.certificate_generator.generator import Generator
+from modules.certificate_generator.utils import create_code
+from modules.google_api.google_api import GoogleAPI
+from modules.google_api.utils import get_certificate_info
