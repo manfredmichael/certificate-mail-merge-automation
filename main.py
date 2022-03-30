9684be32af97805f7dd0e828e4b5b3c8b513052c
manfred michael <koskosanrider@gmail.com>
2022-03-30 09:34:47 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from modules.certificate_sender.sender import Sender
+
+sender = Sender(folder_id='1JVDXhAUC92-Mx553sYWT5tPlbH5UwmIr',
+                client_secret_path='secrets/client_secret.json')
+
+sender.generate_all()
