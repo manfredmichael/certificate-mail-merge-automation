94812650a791f177b42bfcc218e2260d5da30864
manfred michael <koskosanrider@gmail.com>
2022-03-30 09:20:09 +0700

    Add duplicate component exception

--- modules/certificate_sender/utils.py
+++ modules/certificate_sender/utils.py
@@ -12,3 +12,18 @@ class RequiredComponentNotExist(Exception):
         if message is None:
             self.message = f"Required component in {folder} does not exist: {component}" super().__init__(self.message)
         super().__init__(self.message)
+
+class DuplicateComponent(Exception):
+    """Exception raised for errors in the input salary.
+
+    Attributes:
+        folder -- folder in which component duplicated 
+        component -- component which duplicated 
+        message -- explanation of the error
+    """
+
+    def __init__(self, folder, component, message=None):
+        self.message = message
+        if message is None:
+            self.message = f"There are more than one component in {folder} does not exist: {component}"
+        super().__init__(self.message)
